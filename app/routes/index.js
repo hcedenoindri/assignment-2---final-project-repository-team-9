26cdc85c3b70ae5ec44c1696d3474d764b9b0930
hcedenoindri <hcedenoindriago@gmail.com>
2021-10-13 14:26:01 -0500

    edit routes/index.js, README.md

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const port = 3000;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   res.render('index', { title: 'Express' });
 });
 
+console.log(`App listening on port ${port}!`);
 module.exports = router;
