df748be2dc655a5aa6a5589b0e374eba4f2893be
hcedenoindri <hcedenoindriago@gmail.com>
2021-10-13 14:34:09 -0500

    revert index.js, display console meesage from www

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const port = 3000;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-console.log(`App listening on port ${port}!`);
 module.exports = router;
