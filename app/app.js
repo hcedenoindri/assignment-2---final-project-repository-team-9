6a0c5edf3f222af5d246f9831c6c2231b52b543c
hcedenoindri <hcedenoindriago@gmail.com>
2021-10-15 18:18:10 -0500

    implement sign up page view

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', loginRouter);
 app.use('/users', usersRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
