d70e5f753d3077358032670f3b35504fbdc2acb5
hcedenoindri <hcedenoindriago@gmail.com>
2021-10-15 18:24:51 -0500

    merge login.js with loginSubmit.js

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+    
+  var password = req.body.password;
+  var email = req.body.email;
+  var found_flag = false;
+  
+  console.log(" Email: " + email + " Password: " + password);
+
+  'use strict';
+
+  let rawdata = fs.readFileSync('users.json');
+  let users = JSON.parse(rawdata);
+  console.log(users);
+
+  for (var index = 0; index < users.length; ++index) {
+      var user = users[index];
+     
+      if(user.email == email && user.password == password) {
+        found_flag = true;
+        break;
+      }
+  }
+
+  if (found_flag) {
+    // res.render(confirmation, users);
+  }
+  else {
+    var error = "Incorrect email or password";
+    res.render('error', {error: error} );
+  }
 });
 
 module.exports = router;
