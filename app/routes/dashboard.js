508aab4ca02430bce307ff1b50d292d30e6c3aa2
hcedenoindri <hcedenoindriago@gmail.com>
2021-11-12 13:43:38 -0600

    fix views after merge with main

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -6,27 +6,26 @@ router.get('/', function(req, res, next) {
     let userInfo = req.user;
     console.log(req.user);
 
-    setInterval(updatePrices, 60 * 1000) // Update prices every 1 minute
+    setInterval(updatePrices(res, userInfo), 60 * 1000) // Update prices every 1 minute
 });
 
-async function updatePrices() {
+async function updatePrices(res, userInfo) {
     axios.get('https://api.nomics.com/v1/markets?key=2b128e6260e7cd4c7aa84266e91583748dfa6f47')
     .then(function (response) {
-        // // handle success
+        // handle success
         
-        // let qoute = response;
-        // //Math.floor(Math.random() * qoute.length)
-        // let aQoute = qoute.data[Math.floor(Math.random() * qoute.data.length)];
-        // console.log(aQoute);
-        // res.render('login', {data: aQoute });
+        let qoute = response;
+        //Math.floor(Math.random() * qoute.length)
+        let aQoute = qoute.data[Math.floor(Math.random() * qoute.data.length)];
+        console.log(aQoute);
+        res.render('login', {data: aQoute });
     })
     .catch(function (error) {
         // handle error
         console.log(error);
     })
-    .then(function () {
-        res.render('dashboard', {userInfo});
-    });
+
+    res.render('dashboard', {userInfo});
 }
 
 module.exports = router;
